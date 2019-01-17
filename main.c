cc8ba9640c7783fbf133c9bedcaeacb978e98dd2
flagsa <flagsa@qq.com>
2019-01-17 10:56:34 +0800

    增加车辆位置删除功能

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int index=0;
      while (1)
     {
         printf("1---添加车辆位置\n");
-        printf("2---删除车辆位置\n");
+        printf("2---删除车辆位置（最后一辆）\n");
         printf("3---显示全部车辆位置\n");
         printf("4---返回离我的位置最近的车辆\n");
         printf("5---退出\n");
@@ -32,14 +32,35 @@ int index=0;
             scanf("%c",&enter);
             scanf("%c",&enter);
         }
-        if (code==2){}
-        if (code==3){
-            printf("所有车辆位置如下：\n");
-            for(int i=0; i<index;i++)
+        if (code==2){
+            if(index>0)
             {
-                printf("第%d个车辆的位置为(%d,%d)\n",i+1,arr[i].x,arr[i].y);
+                printf("正在删除车辆位置……\n");
+                index--;
+                printf("删除成功，点击回车继续\n");
             }
-            printf("\n点击回车继续\n");
+            else
+            {
+                printf("删除失败，无车辆位置信息，点击回车继续\n");
+            }
+            char enter;
+            scanf("%c",&enter);
+            scanf("%c",&enter);
+        }
+        if (code==3){
+            if(index>0)
+            {
+                printf("所有车辆位置如下：\n");
+                for(int i=0; i<index;i++)
+                {
+                    printf("第%d个车辆的位置为(%d,%d)\n",i+1,arr[i].x,arr[i].y);
+                }
+            }
+            else
+            {
+                printf("无车辆位置信息\n");
+            }
+            printf("点击回车继续\n");
             char enter;
             scanf("%c",&enter);
             scanf("%c",&enter);
